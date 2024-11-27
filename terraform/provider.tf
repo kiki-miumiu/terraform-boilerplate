1b503f8ca2248b529a3d8d79b203e249a1e60360
Brian Huang <brianhuaqiang.huang@cba.com.au>
2024-11-27 19:43:46 +1100

    init

---
+++ terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket  ="terraform-state-<accountid>"
+    key     = "textract/terraform.tfstate"
+    region  = "ap-southeast-2"
+  }
+  required_version = ">= 1.9.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "<= 5.69"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
